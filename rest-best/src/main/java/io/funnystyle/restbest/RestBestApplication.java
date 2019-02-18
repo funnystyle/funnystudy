8a3f6a522175928d093b15649c7a30f7c305ad34
스타마크_김동환 <esero@hanmail.net>
2019-02-19 06:49:15 +0900

    project creation

---
+++ rest-best/src/main/java/io/funnystyle/restbest/RestBestApplication.java
@@ -0,0 +1,13 @@
+package io.funnystyle.restbest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestBestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestBestApplication.class, args);
+    }
+
+}
