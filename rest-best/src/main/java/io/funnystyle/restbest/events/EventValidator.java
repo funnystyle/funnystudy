a8358eccdd4f445a4e6087f88aa3c3913aca850c
스타마크_김동환 <esero@hanmail.net>
2019-02-19 11:04:06 +0900

    validate wrong value

---
+++ rest-best/src/main/java/io/funnystyle/restbest/events/EventValidator.java
@@ -0,0 +1,24 @@
+package io.funnystyle.restbest.events;
+
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+
+import java.time.LocalDateTime;
+
+@Component
+public class EventValidator {
+
+    public void validate(EventDto eventDto, Errors errors) {
+        if (eventDto.getBasePrice() > eventDto.getMaxPrice() && eventDto.getMaxPrice() != 0) {
+            errors.rejectValue("basePrice", "wrongValue", "basePrice is wrong.");
+            errors.rejectValue("maxPrice", "wrongValue", "maxPrice is wrong.");
+        }
+
+        LocalDateTime endEventDateTime = eventDto.getEndEventDateTime();
+        if (endEventDateTime.isBefore(eventDto.getBeginEventDateTime()) ||
+                endEventDateTime.isBefore(eventDto.getBeginEnrollmentDateTime()) ||
+                endEventDateTime.isBefore(eventDto.getCloseEnrollmentDateTime())) {
+            errors.rejectValue("endEventDateTime", "wrongValue", "endEventDateTime is wrong.");
+        }
+    }
+}
--- rest-best/src/test/java/io/funnystyle/restbest/events/EventControllerTests.java
+++ rest-best/src/test/java/io/funnystyle/restbest/events/EventControllerTests.java
@@ -98,4 +98,24 @@ public class EventControllerTests {
                 .content(objectMapper.writeValueAsString(eventDto)))
                 .andExpect(status().isBadRequest());
     }
+
+    @Test
+    public void createEvent_Bad_Request_Wrong_Input() throws Exception {
+        EventDto eventDto = EventDto.builder()
+                .name("Spring")
+                .description("REST API Development with Spring")
+                .beginEnrollmentDateTime(LocalDateTime.of(2018, 11, 25, 14, 21))
+                .closeEnrollmentDateTime(LocalDateTime.of(2018, 11, 24, 14, 21))
+                .beginEventDateTime(LocalDateTime.of(2018, 11, 23, 14, 21))
+                .endEventDateTime(LocalDateTime.of(2018, 11, 22, 14, 21))
+                .basePrice(100)
+                .maxPrice(200)
+                .limitOfEnrollment(100)
+                .build();
+
+        mockMvc.perform(post("/api/events/")
+                .contentType(MediaType.APPLICATION_JSON_UTF8)
+                .content(objectMapper.writeValueAsString(eventDto)))
+                .andExpect(status().isBadRequest());
+    }
 }
